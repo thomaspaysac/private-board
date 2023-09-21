00fac822222f4dcccb8861b64312e9b9e9cea7bb
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-09-21 07:54:26 +0200

    Create basic messages display on index page

--- controllers/messages.js
+++ controllers/messages.js
@@ -3,9 +3,14 @@ const asyncHandler = require("express-async-handler");
 
 const Message = require('../models/message');
 
+
+// Homepage, messages list
 exports.index_get = asyncHandler(async (req, res, next) => {
-  const messages = await Message.find({}).populate('author').exec();
+  const messages = await Message.find({})
+    .sort({ timestamp: -1 })
+    .populate('author')
+    .exec();
   res.render('index', { 
     title: 'Private Board',
     messages });
-})
+});
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const messages_controller = require('../controllers/messages');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const messages = Message.find().populate('author').exec();
-  res.render('index', { 
-    title: 'Private Board',
-    messages });
-});
+router.get('/', messages_controller.index_get);
 
 module.exports = router;
